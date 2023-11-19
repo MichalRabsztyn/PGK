7b22e78c98acd4586a1839d4bfe8f00ce22b6e72
Sajmon101 <szymk4@wp.pl>
2023-11-20 00:46:19 +0100

    Enemies done

--- Wolfenspace/Assets/Scripts/EnemyBrain.cs
+++ Wolfenspace/Assets/Scripts/EnemyBrain.cs
@@ -8,38 +8,107 @@ namespace GDL
 {
     public class EnemyBrain : MonoBehaviour
     {
-        public Transform target;
-
+        public Transform player;
         private EnemyReferences enemyReferences;
 
         private float pathUpdateDeadLine;
 
         private float shootingDistance;
+
+        [SerializeField] float sightRange = 5f;
+
+        [SerializeField] LayerMask whatIsPlayer;
+        [SerializeField] LayerMask obstacleLayer;
+        public bool isChasing = false;
+
+        [Header("Patrolling")]
+        public bool isPatrolling;
+        public GameObject pointA;
+        public GameObject pointB;
+        private float patrolSpeed = 4;
+        private bool whereGo;
+
         private void Awake()
         {
             enemyReferences = GetComponent<EnemyReferences>();
+            pointA.transform.parent = null;
+            pointB.transform.parent = null;
         }
 
         void Start()
         {
             shootingDistance = enemyReferences.navMeshAgent.stoppingDistance;    
+            enemyReferences.navMeshAgent.stoppingDistance = 0;    
         }
 
         void Update()
         {
-            if (target!=null)
+            if (isPatrolling)
             {
-                bool inRange = Vector3.Distance(transform.position, target.position) <= shootingDistance;
+                patrolSpeed = (enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude/15);
+                if (whereGo)
+                {
+                    LookAtTarget(pointA.transform);
+                    UpdatePath(pointA.transform);
+                    enemyReferences.animator.SetFloat("speed", patrolSpeed);
+                    if (Vector3.Distance(pointA.transform.position, transform.position)<1) whereGo = false;
+                }
 
-                if (inRange) LookAtTarget();
-                else UpdatePath();
-
-                enemyReferences.animator.SetBool("shooting", inRange);
+                else
+                {
+                    LookAtTarget(pointB.transform);
+                    UpdatePath(pointB.transform);
+                    enemyReferences.animator.SetFloat("speed", patrolSpeed);
+                    if (Vector3.Distance(pointB.transform.position, transform.position)<1) whereGo = true;
+                }
             }
-            enemyReferences.animator.SetFloat("speed", enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude);
+
+            if(isChasing || CheckPlayerPresence())
+            {
+                if(!isChasing) //things to do once
+                {
+                    pointA.GetComponent<DestroyMyself>().Destroy();
+                    pointB.GetComponent<DestroyMyself>().Destroy();
+                    enemyReferences.navMeshAgent.stoppingDistance = shootingDistance;
+                    isPatrolling = false;
+                    isChasing = true;
+                }
+
+                bool inRange = Vector3.Distance(transform.position, player.position) <= shootingDistance;
+
+                //if (inRange && CanSeePlayer()) LookAtTarget(player);
+                //else UpdatePath(player);
+
+                if (!inRange)
+                {
+                    enemyReferences.navMeshAgent.stoppingDistance = shootingDistance;
+                    UpdatePath(player);
+                    enemyReferences.animator.SetFloat("speed", enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude);
+                    enemyReferences.animator.SetBool("shooting", inRange);
+                }
+
+                else
+                {
+                    LookAtTarget(player);
+                    enemyReferences.animator.SetFloat("speed", enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude);
+                    if (CanSeePlayer())
+                    {
+                        enemyReferences.navMeshAgent.stoppingDistance = shootingDistance;
+                        enemyReferences.animator.SetFloat("speed", enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude);
+                        enemyReferences.animator.SetBool("shooting", inRange);
+                    }
+                    else
+                    {
+                        enemyReferences.navMeshAgent.stoppingDistance -=1;
+                        enemyReferences.animator.SetFloat("speed", enemyReferences.navMeshAgent.desiredVelocity.sqrMagnitude);
+                        UpdatePath(player);
+                    }
+                }
+            }
+
         }
 
-        private void LookAtTarget()
+        private void LookAtTarget(Transform target)
         {
             Vector3 lookPos = target.position - transform.position;
             lookPos.y = 0;
@@ -47,7 +116,7 @@ namespace GDL
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
         }
 
-        private void UpdatePath()
+        private void UpdatePath(Transform target)
         {
             if (Time.time >= pathUpdateDeadLine)
             {
@@ -55,5 +124,35 @@ namespace GDL
                 enemyReferences.navMeshAgent.SetDestination(target.position);
             }
         }
+
+        private bool CheckPlayerPresence()
+        {
+            if (Physics.CheckSphere(transform.position, sightRange, whatIsPlayer))
+            {
+                Vector3 direction = player.position - transform.position;
+
+                Ray ray = new Ray(transform.position, direction);
+
+                float maxDistance = direction.magnitude - 0.5f;
+
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, maxDistance, obstacleLayer)) return false;
+                else return true;
+            }
+
+            return false;
+        }
+
+        bool CanSeePlayer()
+        {
+            Vector3 offset = new Vector3(0, 1.5f, 0);
+            Vector3 directionToPlayer = player.transform.position  - transform.position;
+            
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + offset, directionToPlayer.normalized, out hit, Mathf.Infinity, obstacleLayer)) return false;
+            else return true;
+        }
+
     }
 }
--- Wolfenspace/Assets/Scripts/SneakyShipAttack.cs
+++ Wolfenspace/Assets/Scripts/SneakyShipAttack.cs
@@ -10,19 +10,52 @@ public class SneakyShipAttack : MonoBehaviour
     public LayerMask whatIsPlayer;
     private Vector3 explosionOffset = new Vector3(-0.8f, 1.5f, -1.5f);
     [SerializeField] GameObject particleExplosion;
-    [SerializeField] AudioSource explosionSound;
+
+    [SerializeField] GameObject ExplosionAudio;
+    private AudioPlay explosionSound;
+
+    [Header("Patrolling")]
+    public float patrolSpeed = 5f;
+    public bool isPatrolling;
+    public GameObject pointA;
+    public GameObject pointB;
+    bool whereGo = false;
+    public bool isChasing = false;
 
     public float sightRange;
-    public bool playerInSightRange;
+    bool playerInSightRange;
+    private Animator idleAnim;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        explosionSound = ExplosionAudio.GetComponent<AudioPlay>();
+        pointA.transform.parent = null;
+        pointB.transform.parent = null;
+        idleAnim = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        if (playerInSightRange) ChasePlayer();
+        if (isPatrolling)
+        {
+            idleAnim.enabled = false;
+            Patrol();
+        }
+
+        if (isChasing || CheckPlayerPresence())
+        {
+            if (!isChasing)
+            {
+                idleAnim.enabled = false;
+                isChasing = true;
+
+                pointA.GetComponent<DestroyMyself>().Destroy();
+                pointB.GetComponent<DestroyMyself>().Destroy();
+                isPatrolling = false;
+            }
+
+            ChasePlayer();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -38,12 +71,51 @@ public class SneakyShipAttack : MonoBehaviour
         agent.SetDestination(player.position);
     }
 
-    private void ShipDead()
+    public void ShipDead()
     {
-        explosionSound.Play();
-        GameObject explosion = Instantiate(particleExplosion, transform.position + explosionOffset, transform.rotation);
+        AudioPlay();
         Destroy(gameObject);
+    }
+
+    private void AudioPlay()
+    {
+        explosionSound.PlayAudio();
+        GameObject explosion = Instantiate(particleExplosion, transform.position + explosionOffset, transform.rotation);
+        explosionSound.transform.parent = null;
         Destroy(explosion, 2f);
     }
 
+    private void Patrol()
+    {
+        if (whereGo)
+        {
+            agent.SetDestination(pointA.transform.position);
+            if (Vector3.Distance(pointA.transform.position, transform.position)<0.1) whereGo = false;
+        }
+
+        else
+        {
+            agent.SetDestination(pointB.transform.position);
+            if (Vector3.Distance(pointB.transform.position, transform.position)<0.1) whereGo = true;
+        }
+    }
+
+    private bool CheckPlayerPresence()
+    {
+        if (Physics.CheckSphere(transform.position, sightRange, whatIsPlayer))
+        {
+            Vector3 direction = player.position - transform.position;
+
+            Ray ray = new Ray(transform.position, direction);
+
+            float maxDistance = direction.magnitude - 0.5f;
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxDistance, 7)) return false;
+            else return true;
+        }
+
+        return false;
+    }
 }
